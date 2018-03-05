f48dcd797d17f80d6d10d91157509014f78ecb7d
Justin Mimbs <justin.mimbs@gmail.com>
2018-03-04 20:44:56 -0500

    Initial commit

---
+++ DateParsers.elm
@@ -0,0 +1,168 @@
+module DateParsers
+    exposing
+        ( Date(..)
+        , YearDate(..)
+        , fromIsoString_Parser
+        , fromIsoString_Regex
+        )
+
+import Char
+import Parser exposing ((|.), (|=), Count(Exactly), Parser, andThen, delayedCommitMap, end, fail, ignore, keyword, map, oneOf, source, succeed, symbol)
+import Regex exposing (Regex)
+
+
+type Date
+    = Year Int YearDate
+
+
+type YearDate
+    = MonthDate Int Int
+    | WeekDate Int Int
+    | OrdinalDate Int
+
+
+
+-- Regex
+
+
+fromIsoString_Regex : String -> Result String Date
+fromIsoString_Regex =
+    Regex.find (Regex.AtMost 1) isoDateRegex
+        >> List.head
+        >> Result.fromMaybe "String is not in IS0 8601 date format"
+        >> Result.andThen (.submatches >> fromIsoStringMatches)
+
+
+isoDateRegex : Regex
+isoDateRegex =
+    let
+        year =
+            -- yyyy
+            -- 1
+            "(\\d{4})"
+
+        cal =
+            --       mm            dd
+            -- 2     3             4
+            "(\\-)?(\\d{2})(?:\\2(\\d{2}))?"
+
+        week =
+            --        ww            d
+            -- 5      6             7
+            "(\\-)?W(\\d{2})(?:\\5(\\d))?"
+
+        ord =
+            --     ddd
+            --     8
+            "\\-?(\\d{3})"
+    in
+    Regex.regex <| "^" ++ year ++ "(?:" ++ cal ++ "|" ++ week ++ "|" ++ ord ++ ")?$"
+
+
+fromIsoStringMatches : List (Maybe String) -> Result String Date
+fromIsoStringMatches =
+    let
+        toInt : Maybe String -> Int
+        toInt =
+            Maybe.andThen (String.toInt >> Result.toMaybe) >> Maybe.withDefault 1
+    in
+    \matches ->
+        case matches of
+            [ Just yyyy, _, mn, d, _, wn, wdn, od ] ->
+                Ok <|
+                    Year
+                        (yyyy |> String.toInt |> Result.withDefault 1)
+                        (case ( mn, wn ) of
+                            ( Just _, Nothing ) ->
+                                MonthDate (mn |> toInt) (d |> toInt)
+
+                            ( Nothing, Just _ ) ->
+                                WeekDate (wn |> toInt) (wdn |> toInt)
+
+                            _ ->
+                                OrdinalDate (od |> toInt)
+                        )
+
+            _ ->
+                Err "Unexpected results from isoDateRegex"
+
+
+
+-- Parser
+
+
+fromIsoString_Parser : String -> Result String Date
+fromIsoString_Parser =
+    Parser.run (parseDate |. end) >> Result.mapError (always "String is not in IS0 8601 date format")
+
+
+parseDate : Parser Date
+parseDate =
+    succeed Year
+        |= intFixed 4
+        |= oneOf
+            [ succeed identity
+                |. symbol "-"
+                |= oneOf
+                    [ try <|
+                        succeed MonthDate
+                            |= intFixed 2
+                            |. symbol "-"
+                            |= intFixed 2
+                    , try <|
+                        map OrdinalDate
+                            (intFixed 3)
+                    , succeed MonthDate
+                        |= intFixed 2
+                        |= succeed 1
+                    , succeed WeekDate
+                        |. keyword "W"
+                        |= intFixed 2
+                        |= oneOf
+                            [ succeed identity
+                                |. symbol "-"
+                                |= intFixed 1
+                            , succeed 1
+                            ]
+                    ]
+            , try <|
+                succeed MonthDate
+                    |= intFixed 2
+                    |= intFixed 2
+            , try <|
+                map OrdinalDate
+                    (intFixed 3)
+            , succeed MonthDate
+                |= intFixed 2
+                |= succeed 1
+            , succeed WeekDate
+                |. keyword "W"
+                |= intFixed 2
+                |= oneOf
+                    [ intFixed 1
+                    , succeed 1
+                    ]
+            , succeed (OrdinalDate 1)
+            ]
+
+
+try : Parser a -> Parser a
+try p =
+    delayedCommitMap always p (succeed ())
+
+
+intFixed : Int -> Parser Int
+intFixed width =
+    ignore (Exactly width) Char.isDigit
+        |> source
+        |> andThen (String.toInt >> resultToParser)
+
+
+resultToParser : Result String a -> Parser a
+resultToParser result =
+    case result of
+        Ok x ->
+            succeed x
+
+        Err message ->
+            fail message
---
+++ tests/Tests.elm
@@ -0,0 +1,60 @@
+module Tests exposing (..)
+
+import DateParsers exposing (Date(..), YearDate(..))
+import Expect exposing (Expectation)
+import Test exposing (Test, describe, test)
+
+
+test_fromIsoString : Test
+test_fromIsoString =
+    let
+        toSuite : (String -> Result String Date) -> List Test
+        toSuite fromIsoString =
+            let
+                toTest : ( String, Date ) -> Test
+                toTest ( string, expected ) =
+                    test (string ++ " => " ++ toString expected) <|
+                        \() -> fromIsoString string |> Expect.equal (Ok expected)
+            in
+            [ describe "converts ISO 8601 date strings in basic format" <|
+                List.map toTest
+                    [ ( "2008", Year 2008 <| OrdinalDate 1 )
+                    , ( "200801", Year 2008 <| MonthDate 1 1 )
+                    , ( "200812", Year 2008 <| MonthDate 12 1 )
+                    , ( "20080101", Year 2008 <| MonthDate 1 1 )
+                    , ( "20081231", Year 2008 <| MonthDate 12 31 )
+                    , ( "2009W01", Year 2009 <| WeekDate 1 1 )
+                    , ( "2009W014", Year 2009 <| WeekDate 1 4 )
+                    , ( "2008001", Year 2008 <| OrdinalDate 1 )
+                    , ( "2008061", Year 2008 <| OrdinalDate 61 )
+                    , ( "2008366", Year 2008 <| OrdinalDate 366 )
+                    ]
+            , describe "converts ISO 8601 date strings in extended format" <|
+                List.map toTest
+                    [ ( "2008-01", Year 2008 <| MonthDate 1 1 )
+                    , ( "2008-12", Year 2008 <| MonthDate 12 1 )
+                    , ( "2008-01-01", Year 2008 <| MonthDate 1 1 )
+                    , ( "2008-12-31", Year 2008 <| MonthDate 12 31 )
+                    , ( "2009-W01", Year 2009 <| WeekDate 1 1 )
+                    , ( "2009-W01-4", Year 2009 <| WeekDate 1 4 )
+                    , ( "2008-001", Year 2008 <| OrdinalDate 1 )
+                    , ( "2008-061", Year 2008 <| OrdinalDate 61 )
+                    , ( "2008-366", Year 2008 <| OrdinalDate 366 )
+                    ]
+            , describe "returns Err for malformed date strings" <|
+                List.map
+                    (\s -> test s <| \() -> fromIsoString s |> Expect.equal (Err "String is not in IS0 8601 date format"))
+                    [ "200812-31"
+                    , "2008-1231"
+                    , "2009W01-4"
+                    , "2009-W014"
+                    , "2008-012-31"
+                    , "2008-12-031"
+                    , "2008-0061"
+                    ]
+            ]
+    in
+    describe "fromIsoString"
+        [ describe "Regex" <| toSuite DateParsers.fromIsoString_Regex
+        , describe "Parser" <| toSuite DateParsers.fromIsoString_Parser
+        ]
